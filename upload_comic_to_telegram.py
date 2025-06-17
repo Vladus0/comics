520bf80c6092c88acb1a49f468fcbaa4b6b6b912
Vladus0 <118992340+Vladus0@users.noreply.github.com>
2025-06-17 18:33:30 +0400

    Убрал лишний код

--- upload_comic_to_telegram.py
+++ upload_comic_to_telegram.py
@@ -11,18 +11,13 @@ def main():
     token = os.environ['TELEGRAM_TOKEN']
     chat_id = os.environ["TG_CHAT_ID"]
     bot = telegram.Bot(token=token)
-    parser = argparse.ArgumentParser(description='Эта программа скачивает и загружает комиксы в телеграм')
-    parser.add_argument('--img_path',
-                        type=str, 
-                        default='imgs/img.png',
-                        help="Укажите куда будет сохраняться скачивемый комикс")
-    args = parser.parse_args()
-    alt = get_comic(args.img_path)
+    filepath = "imgs/img.png"
+    alt = get_comic(filepath)
     try:
-        with open(f'{args.img_path}', 'rb') as img:
+        with open(f'{filepath}', 'rb') as img:
             bot.send_photo(chat_id=chat_id, photo=img, caption=alt)
     finally:
-        os.remove(args)
+        os.remove(filepath)
     
         
 if __name__=="__main__":
