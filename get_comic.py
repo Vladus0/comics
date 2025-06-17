40c853e9a4eb7c1cb885c84c1ef4334c791e1e60
Vladus0 <118992340+Vladus0@users.noreply.github.com>
2025-06-17 18:30:24 +0400

    Оптимизировал код, переименовал переменные

--- get_comic.py
+++ get_comic.py
@@ -1,23 +1,23 @@
 import requests
 import random
-import math
 
 
 def get_comic(filepath):
     url = "https://xkcd.com/info.0.json"
     response = requests.get(url=url)
     response.raise_for_status()
-    number_last_comic = response.json()["num"]
-    random_number = math.ceil(random.uniform(0, number_last_comic))
+    last_comic_number = response.json()["num"]
+    random_number = random.randint(0, last_comic_number)
     url = f"https://xkcd.com/{random_number}/info.0.json"
     response = requests.get(url=url)
     response.raise_for_status()
-    img_url = response.json()["img"]
-    img = requests.get(url=img_url)
-    img.raise_for_status()
+    comic_responce = response.json()
+    img_url = comic_responce["img"]
+    comic = requests.get(url=img_url)
+    comic.raise_for_status()
 
-    alt = response.json()["alt"]
+    alt = comic_responce["alt"]
     with open(filepath, 'wb') as file:
-        file.write(img.content)
+        file.write(comic.content)
 
     return alt
